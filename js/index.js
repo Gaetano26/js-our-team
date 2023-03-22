2a35616b4179e129100ca7423c084bfb32f96e9e
Gaetano26 <gaetano.spampinato@outlook.it>
2023-03-22 15:07:18 +0100

    creato array oggetti

---
+++ js/index.js
@@ -0,0 +1,62 @@
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede
+*/
+
+//creo array di oggetti con tutti i membri del team
+const gruppo = [
+    {
+        nome : 'Wayne',
+        cognome : 'Barnett',
+        ruolo : 'Founder & CEO',
+        foto : './img/wayne-barnett-founder-ceo.jpg',
+    },
+       	           
+    {
+        nome: 'Angela',
+        cognome: 'Caroll',
+        ruolo: 'Chief Editor',
+        foto: './img/angela-caroll-chief-editor.jpg',
+    },
+       	    	         
+    {
+        nome: 'Walter',
+        cognome: 'Gordon',
+        ruolo: 'Office Manager',
+        foto:'.img/walter-gordon-office-manager.jpg',
+    },
+       	    	  
+    		              
+  	    	      
+    {
+        nome: 'Angela',
+        cognome: 'Lopez',
+        ruolo: 'Social Media Manager',
+        foto: '.img/angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        nome: 'Scott',
+        cognome: 'Estrada',
+        ruolo: 'Developer',
+        foto :'.img/scott-estrada-developer.jpg',
+    },
+
+    {
+        nome: 'Barbara',
+        cognome: 'Ramos',
+        ruolo: 'Graphic Designer',
+        foto : '.img/barbara-ramos-graphic-designer.jpg',
+    },
+
+];
